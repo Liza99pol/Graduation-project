aa4089ce3c43351c03d745fac0c12da79bb0b010
Liza <102958936+Liza99pol@users.noreply.github.com>
2022-10-06 13:19:24 +0300

    Add files via upload

---
+++ Race/Race/TableRecordsFlow/Cell/NameTableCell.swift
@@ -0,0 +1,16 @@
+//
+//  NameTableCell.swift
+//  Race
+//
+//  Created by macbook m1 on 6.07.22.
+//
+
+import Foundation
+import UIKit
+
+class NameTableCell: UITableViewCell {
+    
+    @IBOutlet var titleCell: UILabel!
+    
+    
+}
